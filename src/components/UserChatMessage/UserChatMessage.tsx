d7311ab0f7c1ad5a206ffb3bed6f121e210df2e8
ganesh vadivel <ganesh.vadivel@covalenseglobal.com>
2024-03-12 13:08:40 +0530

    Added scss changes

--- src/components/UserChatMessage/UserChatMessage.tsx
+++ src/components/UserChatMessage/UserChatMessage.tsx
@@ -1,4 +1,4 @@
-import styles from "./UserChatMessage.module.css";
+import "./UserChatMessage.scss";
 import Avatar from '@mui/material/Avatar';
 import { deepOrange, deepPurple } from '@mui/material/colors';
 import { useSelector } from "react-redux";
@@ -13,8 +13,8 @@ export const UserChatMessage = ({ message }: Props) => {
     const {colorCode} = useSelector((state:any)=>state.theme.color)
 
     return (
-        <div className={styles.container}>
-            <div className={styles.message}>
+        <div className='container'>
+            <div className='message'>
                 <div style={{whiteSpace:"pre-line"}}> {message}</div>
             </div>
             <Avatar sx={{ bgcolor: colorCode, border: `3px solid ${colorCode}` }} alt={userName} src="/" />
