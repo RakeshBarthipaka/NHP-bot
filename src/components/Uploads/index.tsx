fc85745695392e86480ca420a361892fa89d0180
kemburi.bhavanishank <Kemburi.Bhavanishankar@trigyn.com>
2024-03-14 14:48:04 +0530

    upload file view drawer added

--- src/components/Uploads/index.tsx
+++ src/components/Uploads/index.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Uploads.scss";
+
+import { Document, Page, pdfjs } from 'react-pdf';
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import UploadsIcon from "../../assets/images/Uploads.svg";
@@ -14,11 +16,37 @@ import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 import SwapVertIcon from "@mui/icons-material/SwapVert";
 import CloudUploadOutlinedIcon from "@mui/icons-material/CloudUploadOutlined";
 import SearchBar from "../Common/SearchBar";
-import { Button, ButtonGroup } from "@mui/material";
+import { Avatar, Button, ButtonGroup, Drawer, Stack, Toolbar } from "@mui/material";
 import ControlPointIcon from "@mui/icons-material/ControlPoint";
 import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
 import SortOutlinedIcon from "@mui/icons-material/SortOutlined";
+import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
+import ContentCopyOutlinedIcon from "@mui/icons-material/ContentCopyOutlined";
+import StackedBarChart from "../BarChart/BarChart";
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
+import CropOutlinedIcon from '@mui/icons-material/CropOutlined';
+import pdfFile from '../../assets/uploadedFileView.pdf'
+
 const Uploads = (props: any) => {
+    const pdfUrl = pdfFile;
+    const [uploadFileViewer, setUploadFileViewer] = useState(false);
+    const [numPages, setNumPages] = useState<number | null>(null);
+    const [scale, setScale] = useState<number>(1.0);
+    const [containerWidth, setContainerWidth] = useState<number>(250);
+
+    function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
+        setNumPages(numPages);
+    }
+
+    function calculatePageWidth() {
+        // Default width of each page
+        const defaultPageWidth = 540; // You can adjust this value as needed
+        // Calculate the width for the PDF pages based on the container width and scale
+        const pageWidth = containerWidth * (scale || 1);
+        // If the calculated width is less than the default width, use the default width
+        return Math.max(pageWidth, defaultPageWidth);
+    }
+
     const card = (
         <>
             <CardContent className="upload-outer-box">
@@ -37,7 +65,7 @@ const Uploads = (props: any) => {
 
     const fileItems = (
         <>
-            <Box className="file-row">
+            <Box className="file-row" onClick={() => {setUploadFileViewer(true)}}>
                 <Typography sx={{ display: "flex", fontSize: "14px" }}>
                     <PictureAsPdfOutlinedIcon className="pdf-icon" />
                     &nbsp;unilever_chat_1706852987594.pdf
@@ -98,6 +126,61 @@ const Uploads = (props: any) => {
                 {fileItems}
                 {fileItems}
             </Box>
+
+            <Drawer
+                anchor='right'
+                open={uploadFileViewer}
+                onClose={() => setUploadFileViewer(false)}  >
+
+                <div>
+                    <Toolbar className="drawerHeader">
+                        <DescriptionOutlinedIcon />
+                        <Typography variant="h6" noWrap component="div"> Data Source View </Typography>
+                    </Toolbar>
+                    <div className="uploadedFileViewDiv">
+                        <Stack className="stackDataOptions">
+                            <Box display={"flex"} gap={1}>
+                                unilever_chat_1706852987594.pdf
+                            </Box>
+                            <Box display={"flex"} >
+                                <span >
+                                    <Avatar className="iconsBtns">
+                                        <ContentCopyOutlinedIcon />
+                                    </Avatar>
+                                </span>
+
+                                <span >
+                                    <Avatar className="iconsBtns">
+                                        <FileDownloadOutlinedIcon />
+                                    </Avatar>
+                                </span>
+                                <span >
+                                    <Avatar className="iconsBtns">
+                                        <CropOutlinedIcon />
+                                    </Avatar>
+                                </span>
+
+                            </Box>
+                        </Stack>
+                        <div className="uploadedFileView">
+                            <Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess}>
+                                {Array.from(new Array(numPages || 0), (_, index) => (
+                                    <Page
+                                        key={`page_${index + 1}`}
+                                        pageNumber={index + 1}
+                                        width={calculatePageWidth()} // Adjust the width dynamically
+                                        scale={scale}
+                                        // className={styles.pdfPage}
+                                        data-page-number={index + 1}
+                                        customTextRenderer={() => null || ""}
+                                    />
+                                ))}
+                            </Document>
+                        </div>
+                    </div>
+                </div>
+            </Drawer>
+
         </>
     );
 };
