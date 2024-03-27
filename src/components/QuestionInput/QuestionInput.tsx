118d84aa787e10ec590b0b823f7aabe1ed629a19
RakeshBarthipaka <152860307+RakeshBarthipaka@users.noreply.github.com>
2024-03-27 15:16:37 +0530

    Update QuestionInput.tsx

--- src/components/QuestionInput/QuestionInput.tsx
+++ src/components/QuestionInput/QuestionInput.tsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
-import { Stack, TextField } from "@fluentui/react";
+import React, { useEffect, useState } from "react";
+import { Stack } from "@fluentui/react";
 import { Send28Filled, Mic28Filled, Speaker028Filled, SpeakerMute28Filled, ChevronDown20Regular } from "@fluentui/react-icons";
-import "./QuestionInput.scss";
+import styles from "./QuestionInput.module.css";
 import azureSpeakerVoiceList from "../../utils/azureSpeakerVoiceList";
 import { useSelector } from "react-redux";
-
-import VolumeUpOutlinedIcon from '@mui/icons-material/VolumeUpOutlined';
-import VolumeOffOutlinedIcon from '@mui/icons-material/VolumeOffOutlined';
-import KeyboardVoiceOutlinedIcon from '@mui/icons-material/KeyboardVoiceOutlined';
-import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import VolumeUpOutlinedIcon from "@mui/icons-material/VolumeUpOutlined";
+import VolumeOffOutlinedIcon from "@mui/icons-material/VolumeOffOutlined";
+import KeyboardVoiceOutlinedIcon from "@mui/icons-material/KeyboardVoiceOutlined";
+import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
 
 interface Props {
     onSend: (question: string) => void;
@@ -23,21 +24,25 @@ interface Props {
     chatBotVoice: any
 }
 
-
-export const QuestionInput = ({ onSend, disabled, placeholder, clearOnSend, onSelectChatBotTypes, handleMicClick, handleSpeakerClick, isSpeakerOn, isListen, chatBotVoice }: Props) => {
+export const QuestionInput = ({
+    onSend,disabled,placeholder,
+    clearOnSend,
+    onSelectChatBotTypes,
+    handleMicClick,
+    handleSpeakerClick,isSpeakerOn,
+    isListen,chatBotVoice
+}: Props) => {
     const [question, setQuestion] = useState<string>("");
-    const [dropDown, setDropDown] = useState<boolean>(false);
-    const { colorCode, class:themeClass} = useSelector((state:any)=>state.theme.color)
-
+    const [options, setOptions] = useState<any[]>([]);
+    const { colorCode, class: themeClass } = useSelector((state: any) => state.theme.color);
 
     const sendQuestion = () => {
         if (disabled || !question.trim()) {
             return;
         }
-
         onSend(question);
-
         if (clearOnSend) {
+            setOptions([]);
             setQuestion("");
         }
     };
@@ -46,55 +51,82 @@ export const QuestionInput = ({ onSend, disabled, placeholder, clearOnSend, onSe
         if (ev.key === "Enter" && !ev.shiftKey) {
             ev.preventDefault();
             sendQuestion();
+            setQuestion("");
         }
     };
 
-    const onQuestionChange = (_ev: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
+    const onQuestionChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+        const newValue = ev.target.value;
         if (!newValue) {
             setQuestion("");
         } else if (newValue.length <= 1000) {
             setQuestion(newValue);
         }
     };
-
     const sendQuestionDisabled = disabled || !question.trim();
+    const makeApiCall = async () => {
+        try {
+             const response = await fetch(`${question}`);
+            // const response = await fetch(`http://20.193.133.240:8544/autocomplete?question=${question}&closest_matches=5`);
+            const data = await response.json();
+            if (data.results) {
+                setOptions(data.results.map((result: any) => ({ label: result })));
+            }
+        } catch (error) {
+            console.log("Error", error);
+        }
+    };
+    useEffect(() => {
+        makeApiCall();
+    }, [question]);
 
     return (
-        <Stack horizontal className='questionInputContainer'>
-          
-            <TextField
-                className={`questionInputTextArea chatboxTourGuide`}
-                placeholder={placeholder}
-                resizable={false}
-                borderless
-                value={question}
-                onChange={onQuestionChange}
-                onKeyDown={onEnterPress}
+        <Stack horizontal className={`${styles.questionInputContainer}`} style={{ border: `1px solid ${colorCode}` }}>
+            <Autocomplete
+                className={`${styles.questionInputTextArea} chatboxTourGuide`}
+                sx={{
+                    "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "transparent" // Make the border transparent
+                    },
+                    "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "transparent" // Make the border transparent on hover
+                    },
+                    "&.Mui-focused .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "transparent" // Make the border transparent when focused
+                    }
+                }}
+                id="combo-box-demo"
+                options={options}
+                onChange={(event, newValue) => {
+                    if (newValue) {
+                        setQuestion(newValue.label);
+                    }
+                }}
+                renderInput={params => (
+                    <TextField {...params} placeholder={placeholder} onChange={onQuestionChange} onKeyDown={onEnterPress} value={question} />
+                )}
             />
 
-            <div className={`questionInputMicSpeakerBtn speechServiceTourGuide`}>
+            <div className={`${styles.questionInputMicSpeakerBtn} speechServiceTourGuide`}>
                 {isListen ? (
-                    // <Mic28Filled className={`${styles.microphoneRecognize} ${themeClass}`} onClick={handleMicClick} />
-                    <KeyboardVoiceOutlinedIcon className='microphoneRecognize}' onClick={handleMicClick}  />
-                ) : <Mic28Filled primaryFill={colorCode} onClick={handleMicClick} />
-                }
+                    <KeyboardVoiceOutlinedIcon className={`${styles.microphoneRecognize}`} onClick={handleMicClick} />
+                ) : (
+                    <Mic28Filled primaryFill={colorCode} onClick={handleMicClick} />
+                )}
                 {isSpeakerOn ? (
-                    // <Speaker028Filled className={`${styles.speakersoundAnimation} ${themeClass}`} onClick={handleSpeakerClick} />
-                    <VolumeUpOutlinedIcon className='speakersoundAnimation' onClick={handleSpeakerClick}/>
-                ) : 
-                // <SpeakerMute28Filled primaryFill={colorCode} onClick={handleSpeakerClick} />
-                <VolumeOffOutlinedIcon sx={{color: colorCode}} onClick={handleSpeakerClick}/>
-                }
+                    <VolumeUpOutlinedIcon className={`${styles.speakersoundAnimation}`} onClick={handleSpeakerClick} />
+                ) : (
+                    <VolumeOffOutlinedIcon sx={{ color: colorCode }} onClick={handleSpeakerClick} />
+                )}
             </div>
 
-            <div className='questionInputButtonsContainer'>
+            <div className={styles.questionInputButtonsContainer}>
                 <div
-                    className={`questionInputSendButton ${sendQuestionDisabled ? 'questionInputSendButtonDisabled' : ""}`}
+                    className={`${styles.questionInputSendButton} ${sendQuestionDisabled ? styles.questionInputSendButtonDisabled : ""}`}
                     aria-label="Ask question button"
-                    onClick={sendQuestion}
+                    onClick={() => {sendQuestion}}
                 >
-                    {/* <Send28Filled primaryFill={colorCode} /> */}
-                    <SendOutlinedIcon className="sendIcon"/>
+                    <SendOutlinedIcon sx={{ color: "#fff" }} />
                 </div>
             </div>
         </Stack>
