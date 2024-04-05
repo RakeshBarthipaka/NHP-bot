8166c09c9f324431b5ba9e0b44f441833dc7fd74
KVLAmrutha1807 <amrutha.karpuram@covalenseglobal.com>
2024-04-05 13:09:07 +0530

    Added chat endpoint

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from fastapi import FastAPI, HTTPException
-import os
-import time
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
 from llama_index.core import StorageContext, VectorStoreIndex, load_index_from_storage, SimpleDirectoryReader
 from llama_index.core import PromptTemplate
 from llama_index.llms.openai import OpenAI
 from llama_index.core.prompts import LangchainPromptTemplate
 from langchain import hub
-from IPython.display import Markdown,display
-
+from datetime import datetime
+import os
+import time
 
 os.environ["OPENAI_API_KEY"] = "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"
 
@@ -35,7 +35,6 @@ def load_data():
     report_pdf = SimpleDirectoryReader("data").load_data()
     return report_pdf
 
-
 gpt35_llm = OpenAI(model="gpt-3.5-turbo")
 
 if not os.path.exists("test_data_embeddings"):
@@ -46,17 +45,16 @@ else:
 
 report_engine = report_index.as_query_engine(response_mode="tree_summarize", similarity_top_k=4, llm=gpt35_llm)
 
-
 new_summary_tmpl_str = (
-"Context information is below.\n"
-"---------------------\n"
-"{context_str}\n"
-"---------------------\n"
-"Given the context information and not prior knowledge\n"
-"First answer the user query\n"
-"Then give atmost 3 recommended products similar to the user query\n"
-"Query: {query_str}\n"
-"Answer: "
+    "Context information is below.\n"
+    "---------------------\n"
+    "{context_str}\n"
+    "---------------------\n"
+    "Given the context information and not prior knowledge\n"
+    "First answer the user query\n"
+    "Then give at most 3 recommended products similar to the user query\n"
+    "Query: {query_str}\n"
+    "Answer: "
 )
 
 new_summary_tmpl = PromptTemplate(new_summary_tmpl_str)
@@ -75,12 +73,35 @@ report_engine.update_prompts(
 
 app = FastAPI()
 
-@app.get("/answer/")
-async def get_answer(question: str):
+@app.post("/chat")
+async def chat(payload: dict):
     try:
-        result = report_engine.query(question)
+        user_input_history = payload.get("history", [])
+        userID = payload.get("userID", "")
+        if not user_input_history:
+            return JSONResponse(
+                content={"error": "User input history is missing"}, status_code=400
+            )
+
+        user_input_str = user_input_history[-1].get("user", "")
+        result = report_engine.query(user_input_str)
         result = str(result)
-        return result
+
+        formatted_response = {
+            "data_points": [user_input_str],
+            "thoughts": "thoughts",
+            "exchange_id": 0,
+            "question": user_input_str,
+            "answer": result,
+            "tokens": 0,
+            "recommended_question": [],
+            "isChartRequired": False,
+            "chart_data": None,
+            "query_id": ""
+        }
+
+        return JSONResponse(content=formatted_response, status_code=200)
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
